248e894de0cb57fdda1fa17a7922877e4e5207f1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 20:24:27 -0300

    config api

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+app.get('/cep/:cep', async (req, res) => {
+    const { cep } = req.params;
+
+    try {
+        const response = await axios.get(`https://viacep.com.br/ws${cep}/json/`);
+        const endereco = response.data;
+
+        if (endereco.erro) {
+            return res.status(404).json({ mensagem: 'CEP não encontrado' });
+        }
+
+        res.json({
+            cep: endereco.cep,
+            logradouro: endereco.logradouro,
+            bairro: endereco.bairro,
+            cidade: endereco.localidade,
+            estado: endereco.uf
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: 'Erro ao consultar o CEP' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
